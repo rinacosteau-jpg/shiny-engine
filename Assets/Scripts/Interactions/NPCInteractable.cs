de95528670fb2765673a1bd5362edffb8d0f8027
rinacosteau-jpg <rina.costeau@gmail.com>
2025-08-21 16:17:14 +0300

    Refactor interactables with OOP

---
+++ Assets/Scripts/Interactions/NPCInteractable.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// NPC implementation of a dialogue interactable.
+/// Currently does not add extra behaviour but allows future extensions.
+/// </summary>
+public class NPCInteractable : DialogueInteractable
+{
+}
--- Assets/Scripts/PlayerInteractScript.cs
+++ Assets/Scripts/PlayerInteractScript.cs
@@ -13,8 +13,9 @@ public class PlayerInteractScript : MonoBehaviour {
             float interactRange = 2f;
             Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
             foreach (Collider collider in colliderArray) {
-                if (collider.TryGetComponent(out NPCInteractable npcInteractable)) {
-                    npcInteractable.Interact();
+                var interactable = collider.GetComponent(typeof(IInteractable)) as IInteractable;
+                if (interactable != null) {
+                    interactable.Interact();
                 }
             }
         }
