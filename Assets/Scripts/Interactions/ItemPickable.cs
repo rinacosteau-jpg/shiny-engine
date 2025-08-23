63ed2650b7ee1e6334cc38fed925f0857eadbd0d
rinacosteau-jpg <rina.costeau@gmail.com>
2025-08-23 13:09:58 +0300

    Add item pickup and loop reset inventory

--- Assets/Scripts/Interactions/ItemPickable.cs
+++ Assets/Scripts/Interactions/ItemPickable.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 
-public class ItemPickable : MonoBehaviour
+public class ItemPickable : MonoBehaviour, IInteractable, ILoopResettable
 {
     [SerializeField] private string itemID;
     public bool isPicked = false;
+
+    public void Interact()
+    {
+        if (isPicked)
+            return;
+
+        InventoryStorage.Add(new Item(itemID));
+        isPicked = true;
+        gameObject.SetActive(false);
+    }
+
+    public void OnLoopReset()
+    {
+        isPicked = false;
+        gameObject.SetActive(true);
+    }
 }
