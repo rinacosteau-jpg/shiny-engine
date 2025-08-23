3d064dd6cb008ec2d3f3c49d6b81540f465e9b3c
rinacosteau-jpg <rina.costeau@gmail.com>
2025-08-23 13:34:40 +0300

    Reset ILoopResettable objects dynamically on loop reset

--- Assets/Scripts/LoopResetInputScript.cs
+++ Assets/Scripts/LoopResetInputScript.cs
@@ -1,28 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections.Generic;
 
 public class LoopResetInputScript : MonoBehaviour {
     private InputAction resetAction;
-    private ILoopResettable[] resettableObjects;
 
     void Start() {
         resetAction = InputSystem.actions.FindAction("Reset");
 
-        var monoBehaviours = FindObjectsOfType<MonoBehaviour>(true);
-        var list = new List<ILoopResettable>();
-        foreach (var mb in monoBehaviours) {
-            if (mb is ILoopResettable resettable) {
-                list.Add(resettable);
-            }
-        }
-        resettableObjects = list.ToArray();
     }
 
     public void LoopReset()
     {
-        foreach (var resettable in resettableObjects) {
-            resettable.OnLoopReset();
+        var monoBehaviours = FindObjectsOfType<MonoBehaviour>(true);
+        foreach (var mb in monoBehaviours) {
+            if (mb is ILoopResettable resettable) {
+                resettable.OnLoopReset();
+            }
         }
 
         InventoryStorage.Clear();
