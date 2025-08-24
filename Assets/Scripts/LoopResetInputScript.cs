2bf8768faad68d21f6e06ba1846c758869d5566c
rinacosteau-jpg <rina.costeau@gmail.com>
2025-08-24 12:29:05 +0300

    Implement comprehensive loop reset

--- Assets/Scripts/LoopResetInputScript.cs
+++ Assets/Scripts/LoopResetInputScript.cs
@@ -11,16 +11,22 @@ public class LoopResetInputScript : MonoBehaviour {
 
     public void LoopReset()
     {
+        QuestManager.ResetTemporary();
+
+        GameTime.Instance.Hours = 12;
+        GameTime.Instance.Minutes = 12;
+
+        if (GlobalVariables.Instance == null || !GlobalVariables.Instance.player.hasArtifact)
+        {
+            InventoryStorage.Clear();
+        }
+
         var monoBehaviours = FindObjectsOfType<MonoBehaviour>(true);
         foreach (var mb in monoBehaviours) {
             if (mb is ILoopResettable resettable) {
                 resettable.OnLoopReset();
             }
         }
-
-        InventoryStorage.Clear();
-        GameTime.Instance.Hours = 12;
-        GameTime.Instance.Minutes = 12;
 
     }
 
