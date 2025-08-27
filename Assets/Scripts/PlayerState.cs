cce7192a46711c5730c04a3c29c11bb72e0f96e8
Rina <rina.costeau@gmail.com>
2025-08-27 15:39:24 +0300

    saved-lol

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -6,17 +6,19 @@ public struct PlayerState {
     public bool hasArtifact;
     public bool hasGun;
 
-    public int moral;
+    public int moralVal;
+    public int moralCap;
 
     public Skill skillPersuasion;
     public Skill skillPerseption;
     public Skill skillAccuracy;
 
-    public PlayerState(HashSet<string> knowledge = null, bool hasArtifact = false, bool hasGun = false, int moral=10) {
+    public PlayerState(HashSet<string> knowledge = null, bool hasArtifact = false, bool hasGun = false, int moralVal=10, int moralCap =10) {
         Knowledge = knowledge ?? new HashSet<string>();
         this.hasArtifact = hasArtifact;
         this.hasGun = hasGun;
-        this.moral = moral;
+        this.moralVal = moralVal;
+        this.moralCap = moralCap;
         skillPersuasion = new Skill("Persuasion");
         skillPerseption = new Skill("Perseption");
         skillAccuracy = new Skill("Accuracy");
