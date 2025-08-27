c37c02fee3360afd45812fc13d7995de2a90ef67
rinacosteau-jpg <rina.costeau@gmail.com>
2025-08-27 18:31:16 +0300

    Revert "Force Articy flow recalculation after skill checks"

--- Assets/Scripts/DialogueSystem/DialogueUI.cs
+++ Assets/Scripts/DialogueSystem/DialogueUI.cs
@@ -71,15 +71,6 @@ public class DialogueUI : MonoBehaviour, IArticyFlowPlayerCallbacks, ILoopResett
         }
     }
 
-    public void ForceRecalculateBranches()
-    {
-        TryInitReflection();
-        if (recalcMethod != null)
-            recalcMethod.Invoke(flowPlayer, null);
-        if (variableCacheField != null)
-            variableCacheField.SetValue(null, false);
-    }
-
     private void Update()
     {
         if (!IsDialogueOpen)
