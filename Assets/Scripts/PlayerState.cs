fc3c663581849b7d65d7077c1cdecd60a69b6809
rinacosteau-jpg <rina.costeau@gmail.com>
2025-08-27 15:36:37 +0300

    Revert "Sync moral data with Articy globals"

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -6,19 +6,17 @@ public struct PlayerState {
     public bool hasArtifact;
     public bool hasGun;
 
-    public int moralCap;
-    public int moralVal;
+    public int moral;
 
     public Skill skillPersuasion;
     public Skill skillPerseption;
     public Skill skillAccuracy;
 
-    public PlayerState(HashSet<string> knowledge = null, bool hasArtifact = false, bool hasGun = false, int moralCap = 10, int moralVal = 10) {
+    public PlayerState(HashSet<string> knowledge = null, bool hasArtifact = false, bool hasGun = false, int moral=10) {
         Knowledge = knowledge ?? new HashSet<string>();
         this.hasArtifact = hasArtifact;
         this.hasGun = hasGun;
-        this.moralCap = moralCap;
-        this.moralVal = moralVal;
+        this.moral = moral;
         skillPersuasion = new Skill("Persuasion");
         skillPerseption = new Skill("Perseption");
         skillAccuracy = new Skill("Accuracy");
